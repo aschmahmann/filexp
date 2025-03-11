51d6912870472e31001c077e36ce32f855e9a67e
Peter Rabbitson <ribasushi@leporine.io>
2025-03-11 20:22:52 +0100

    Allow force-specifying a particular tipset, regardless of blocksource

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -169,65 +170,91 @@ func main() {
 const chainLoveURL = "https://api.chain.love/"
 
 func getAnchorPoint(cctx *cli.Context) (*blockGetter, *lchtypes.TipSet, error) {
-	sourceSelect := []string{"car", "tipset-cids", "rpc-endpoint", "rpc-fullnode"}
+	sourceSelect := []string{"car", "rpc-endpoint", "rpc-fullnode"}
 
-	var isSet int
+	var countHeadSources int
 	for _, s := range sourceSelect {
 		if cctx.IsSet(s) {
-			isSet++
+			countHeadSources++
 		}
 	}
-	if isSet == 0 && cctx.Bool("trust-chainlove") {
-		isSet++
+	if countHeadSources == 0 && cctx.Bool("trust-chainlove") {
+		countHeadSources++
 		if err := cctx.Set("rpc-endpoint", chainLoveURL); err != nil {
 			return nil, nil, err
 		}
 	}
 
-	if isSet != 1 {
-		return nil, nil, xerrors.Errorf("you must specify exactly one of: %s", strings.Join(sourceSelect, ", "))
+	if countHeadSources > 1 {
+		return nil, nil, xerrors.Errorf(
+			"you can not specify more than one CurrentTipsetSource of: %s",
+			strings.Join(stringSliceMap(sourceSelect, func(s string) string { return "--" + s }), ", "),
+		)
+	} else if countHeadSources == 0 && !cctx.IsSet("tipset-cids") {
+		return nil, nil, xerrors.Errorf(
+			"you have not specified any CurrentTipsetSource (one of %s), as an alternative you must provide the tipset explicitly via '--tipset-cids'",
+			strings.Join(stringSliceMap(sourceSelect, func(s string) string { return "--" + s }), ", "),
+		)
+	}
+
+	rpcAddr := cctx.String("rpc-fullnode")
+	if rpcAddr == "" {
+		rpcAddr = cctx.String("rpc-endpoint")
 	}
 
 	ctx := cctx.Context
 	var err error
 	var bg *blockGetter
-	var tsk lchtypes.TipSetKey
+	var tsk *lchtypes.TipSetKey
 	var ts *lchtypes.TipSet
 
-	if cctx.IsSet("car") {
-		bg, tsk, err = getStateFromCar(ctx, cctx.String("car"))
-		if err != nil {
-			return nil, nil, err
-		}
-	} else if cctx.IsSet("tipset-cids") {
+	// supplied TSK takes precedence
+	if cctx.IsSet("tipset-cids") {
 		cidStrs := cctx.StringSlice("tipset-cids")
 		var cids []cid.Cid
 		for _, s := range cidStrs {
-			c, err := cid.Decode(s)
-			if err != nil {
-				return nil, nil, err
+			// urfave is dumb wrt multi-value flags, do some postprocessing
+			for _, ss := range regexp.MustCompile(`[\s,:;]`).Split(s, -1) {
+				if ss == "" {
+					continue
+				}
+				c, err := cid.Decode(ss)
+				if err != nil {
+					return nil, nil, err
+				}
+				cids = append(cids, c)
 			}
-			cids = append(cids, c)
 		}
-		tsk = lchtypes.NewTipSetKey(cids...)
-	} else {
+		tskv := lchtypes.NewTipSetKey(cids...)
+		tsk = &tskv
+	}
 
-		rpcAddr := cctx.String("rpc-fullnode")
-		if rpcAddr == "" {
-			rpcAddr = cctx.String("rpc-endpoint")
+	if cctx.IsSet("car") {
+		var carTsk *lchtypes.TipSetKey
+		bg, carTsk, err = getStateFromCar(ctx, cctx.String("car"))
+		if err != nil {
+			return nil, nil, err
 		}
-
+		// not forced via --tipset-cids
+		if tsk == nil {
+			tsk = carTsk
+		}
+	} else if rpcAddr != "" {
 		lApi, apiCloser, err := fil.NewLotusDaemonAPIClientV0(ctx, rpcAddr, 0, "")
 		if err != nil {
 			return nil, nil, err
 		}
 		defer apiCloser()
 
-		ts, err = fil.GetTipset(ctx, lApi, filabi.ChainEpoch(cctx.Uint("lookback-epochs")))
-		if err != nil {
-			return nil, nil, err
+		// not forced via --tipset-cids
+		if tsk == nil {
+			ts, err = fil.GetTipset(ctx, lApi, filabi.ChainEpoch(cctx.Uint("lookback-epochs")))
+			if err != nil {
+				return nil, nil, err
+			}
 		}
 
+		// only a full mode RPC can act as a block source
 		if cctx.IsSet("rpc-fullnode") {
 			bg = &blockGetter{
 				m:              make(map[cid.Cid]int),
@@ -236,6 +263,7 @@ func getAnchorPoint(cctx *cli.Context) (*blockGetter, *lchtypes.TipSet, error) {
 		}
 	}
 
+	// if no block sources available - fall back to public bitswap
 	if bg == nil {
 		bg, err = initBitswapGetter(ctx)
 		if err != nil {
