4b3187ec3fe40190406d481aa29898d5138fb30e
Peter Rabbitson <ribasushi@leporine.io>
2025-07-14 13:12:45 +0200

    Optimize f05 dumping: apparently folks use this in prod now 🪦
    Processing ~70 million entries using an inefficient "swarm of goroutines" puts
    exortbitant pressure on GC, especially notable in low-memory environments.
    
    While one can not solve all of the problems (due to low-level IPLD libs being
    meh), a number of reductions can help:
    
    - switch to a static number of consumer-workers
    - reuse a single static encode-buffer per worker (json.Encode does not help)
    - keep as much of possible on the stack
    - reuse the variables within loops where it makes sense
    - pass the from-IPLD struct by ref where possible
    - use a single mutex syncpoint, remove atomic incs entirely

--- internal/state/f05.go
+++ internal/state/f05.go
@@ -2,12 +2,13 @@ package state
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"runtime"
-	"sync/atomic"
+	"sync"
 
 	"github.com/aschmahmann/filexp/internal/ipld"
 	filabi "github.com/filecoin-project/go-state-types/abi"
@@ -19,36 +20,15 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-type MarketDealState struct {
-	SectorNumber     filabi.SectorNumber
-	SectorStartEpoch filabi.ChainEpoch
-	LastUpdatedEpoch filabi.ChainEpoch
-	SlashEpoch       filabi.ChainEpoch
-}
-type JsonEntry struct {
-	DealID   *filabi.DealID `json:",omitempty"`
-	Proposal lchmarket.DealProposal
-	State    MarketDealState
+type deal struct {
+	dealID   filabi.DealID
+	proposal *lchmarket.DealProposal
 }
 
-func DumpStateF05(ctx context.Context, bg *ipld.CountingBlockGetter, ts *lchtypes.TipSet, outFh io.Writer, asSingleDocument bool) error {
-	// POSIX pipe writes are not atomic after certain size, we need a synchronizer not to tear the json
-	// run the worker in an outer errgroup to allow for all producers to shut down first
-	writeSink := make(chan []byte, 8<<10)
-	egOuter, ctx := errgroup.WithContext(ctx)
-	egOuter.Go(func() error { return writeWorker(ctx, writeSink, outFh, asSingleDocument) })
-
-	egInner, ctx := errgroup.WithContext(ctx)
-	wrkCnt := runtime.NumCPU()
-	if wrkCnt < 3 {
-		wrkCnt = 3 // one iterator, and at least two encoders
-	} else if wrkCnt > 12 {
-		wrkCnt = 12 // do not overwhelm the block provider
-	}
-	egInner.SetLimit(wrkCnt)
+func DumpStateF05(ctx context.Context, bg *ipld.CountingBlockGetter, ts *lchtypes.TipSet, outFh io.Writer, asSingleDocument bool) (defErr error) {
 
 	//
-	// begin actual chain-reading logic
+	// Setup various chain access
 	//
 	cbs := ipldcbor.NewCborStore(bg)
 
@@ -61,139 +41,167 @@ func DumpStateF05(ctx context.Context, bg *ipld.CountingBlockGetter, ts *lchtype
 		return err
 	}
 
-	proposals, err := f05state.Proposals()
+	proposalsReader, err := f05state.Proposals()
 	if err != nil {
 		return err
 	}
 
-	states, err := f05state.States()
+	statesReader, err := f05state.States()
 	if err != nil {
 		return err
 	}
 
-	egInner.Go(func() error {
-		var cnt atomic.Int64
-
-		// Currently this takes ~2 minutes for a `return nil` noop via a car file 🪦
-		// It should take ~10 seconds instead (based on napking math over tree size, 2.7M blocks)
-		//
-		// The reason for the discrepancy is lack of a counterpart parallelIterateArray modeled on
-		// https://github.com/aschmahmann/filexp/blob/6f5f5d16f7e/internal/ipld/adl.go#L22-L36
-		// to then be able to burn through these 2 arrays in parallel and "zip" them up
-		// https://github.com/filecoin-project/builtin-actors/blob/v15.0.0/actors/market/src/state.rs#L40-L48
-		//
-		// Blockers are this PR and its deps: https://github.com/filecoin-project/go-amt-ipld/pull/84
-		//
-		return proposals.ForEach(func(did filabi.DealID, dp lchmarket.DealProposal) error {
-
-			// keep count, also to deal with trailing comma in case of asSingleDocument
-			isFirst := (cnt.Add(1) == 1)
-
-			egInner.Go(func() error {
-
-				// https://github.com/filecoin-project/lotus/blob/v1.30.0/chain/actors/builtin/market/market.go#L306-L320
-				mds := MarketDealState{
-					SectorNumber:     0,
-					SectorStartEpoch: -1,
-					LastUpdatedEpoch: -1,
-					SlashEpoch:       -1,
-				}
-				s, found, err := states.Get(did)
-				if err != nil {
-					return err
-				}
-				if found {
-					mds.SectorNumber = s.SectorNumber()
-					mds.SectorStartEpoch = s.SectorStartEpoch()
-					mds.LastUpdatedEpoch = s.LastUpdatedEpoch()
-					mds.SlashEpoch = s.SlashEpoch()
-				}
-
-				// if we do end up with proper concurrency (see comment above), dealing with pooled allocs will be worthwhile
-				// right now it's definitively a wash
-				toEnc := JsonEntry{
-					Proposal: dp,
-					State:    mds,
-				}
-				if !asSingleDocument {
-					toEnc.DealID = &did
-				}
-
-				enc, err := json.Marshal(toEnc)
-				if err != nil {
-					return err
-				}
-
-				encFin := make([]byte, 0, len(enc)+15)
-
-				if asSingleDocument {
-					if !isFirst {
-						encFin = append(encFin, ","...)
-					}
-					encFin = append(encFin, fmt.Sprintf(`"%d":`, did)...)
-					encFin = append(encFin, enc...)
-				} else {
-					encFin = append(encFin, enc...)
-					encFin = append(encFin, "\n"...)
-				}
-
-				select {
-				case <-ctx.Done():
-					return nil
-				case writeSink <- encFin:
-				}
-
-				return nil
-			})
-			return nil
-		})
-	})
-
-	innerErr := egInner.Wait()
-	close(writeSink)
-	outerErr := egOuter.Wait()
-
-	if innerErr != nil {
-		return innerErr
-	} else if outerErr != nil {
-		return outerErr
-	}
-
-	return nil
-}
-
-func writeWorker(ctx context.Context, in <-chan []byte, out io.Writer, asSingleDocument bool) (defErr error) {
-
-	buf := bufio.NewWriterSize(out, 1<<20)
+	//
+	// Setup output
+	//
+	outBuf := bufio.NewWriterSize(outFh, 1<<20) // 1 MiB buffer, because why not
 	defer func() {
 		if defErr == nil {
-			defErr = buf.Flush()
+			defErr = outBuf.Flush()
 		}
 	}()
 
 	if asSingleDocument {
-		if _, err := buf.Write([]byte(`{`)); err != nil {
+		if _, err := outBuf.WriteString(`{`); err != nil {
 			return err
 		}
 		defer func() {
-			_, err := buf.Write([]byte("}\n"))
+			_, err := outBuf.WriteString("}\n")
 			if defErr == nil {
 				defErr = err
 			}
 		}()
 	}
 
-	for {
-		select {
-		case b, isOpen := <-in:
-			if !isOpen {
-				return nil
+	//
+	// Setup emitter workers
+	//
+	wrkCnt := runtime.NumCPU() - 1
+	if wrkCnt < 1 {
+		wrkCnt = 1
+	} else if wrkCnt > 12 {
+		wrkCnt = 12 // do not overwhelm the block provider
+	}
+
+	deals := make(chan deal, 16*wrkCnt)
+	notFirst := new(bool)
+
+	// Neither stdlib bufio, nor POSIX pipe writes are atomic
+	// need a synchronizer either way not to tear the JSON
+	mu := new(sync.Mutex)
+
+	// this ctx is not passed to workers: they are shut down on closed input channel alone
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for i := wrkCnt; i > 0; i-- {
+		eg.Go(func() error { return dealEmitter(asSingleDocument, deals, statesReader, outBuf, mu, notFirst) })
+	}
+
+	//
+	// Actual chain-reading logic
+	//
+	// Currently this takes ~2 minutes for a `return nil` noop via a car file 🪦
+	// It should take ~10 seconds instead (based on napking math over tree size, 2.7M blocks)
+	//
+	// The reason for the discrepancy is lack of a counterpart parallelIterateArray modeled on
+	// https://github.com/aschmahmann/filexp/blob/6f5f5d16f7e/internal/ipld/adl.go#L22-L36
+	// to then be able to burn through these 2 arrays in parallel and "zip" them up
+	// https://github.com/filecoin-project/builtin-actors/blob/v15.0.0/actors/market/src/state.rs#L40-L48
+	//
+	// Blockers are this PR and its deps: https://github.com/filecoin-project/go-amt-ipld/pull/84
+	//
+	eg.Go(func() error {
+		// if any of the consumers of chan deals errors out, context below is cancelled, so the channeld is closed, so the consumers shut down
+		defer close(deals)
+		return proposalsReader.ForEach(func(did filabi.DealID, dp lchmarket.DealProposal) error {
+			select {
+			case <-ctx.Done():
+			case deals <- deal{did, &dp}:
 			}
-			if _, err := buf.Write(b); err != nil {
-				return err
-			}
-		case <-ctx.Done():
 			return nil
+		})
+	})
+
+	return eg.Wait()
+}
+
+func dealEmitter(asSingleDoc bool, deals <-chan deal, stateReader lchmarket.DealStates, out *bufio.Writer, mu *sync.Mutex, notFirst *bool) error {
+
+	// inline definition only here for clarity
+	// reuse struct across invocations - it has sufficiently low amount of fields that resetting everything is safe/fast
+	var toEnc struct {
+		DealID   *filabi.DealID `json:",omitempty"`
+		Proposal *lchmarket.DealProposal
+		State    struct {
+			SectorNumber     filabi.SectorNumber
+			SectorStartEpoch filabi.ChainEpoch
+			LastUpdatedEpoch filabi.ChainEpoch
+			SlashEpoch       filabi.ChainEpoch
+		}
+	}
+
+	jBuf := bytes.NewBuffer(make([]byte, 0, 1024)) // just a starting point, the buffer will self-grow if needed
+	jEnc := json.NewEncoder(jBuf)
+
+	for {
+		d, isOpen := <-deals
+		if !isOpen {
+			return nil
+		}
+
+		jBuf.Reset()
+
+		if asSingleDoc {
+			jBuf.WriteString(fmt.Sprintf(`"%d":`, d.dealID))
+		} else {
+			toEnc.DealID = &d.dealID
+		}
+
+		toEnc.Proposal = d.proposal
+
+		s, found, err := stateReader.Get(d.dealID)
+		if err != nil {
+			return err
+		}
+		if found {
+			toEnc.State.SectorNumber = s.SectorNumber()
+			toEnc.State.SectorStartEpoch = s.SectorStartEpoch()
+			toEnc.State.LastUpdatedEpoch = s.LastUpdatedEpoch()
+			toEnc.State.SlashEpoch = s.SlashEpoch()
+		} else {
+			// https://github.com/filecoin-project/lotus/blob/v1.30.0/chain/actors/builtin/market/market.go#L306-L320
+			toEnc.State.SectorNumber = 0
+			toEnc.State.SectorStartEpoch = -1
+			toEnc.State.LastUpdatedEpoch = -1
+			toEnc.State.SlashEpoch = -1
+		}
+
+		if err := jEnc.Encode(toEnc); err != nil {
+			return err
+		}
+
+		toWrite := jBuf.Bytes()
+
+		mu.Lock()
+		if asSingleDoc {
+			// write out the "leading" comma
+			if *notFirst {
+				_, err = out.WriteRune(',')
+			} else {
+				*notFirst = true
+			}
+
+			// strip the newline inserted by Encode() above
+			toWrite = toWrite[:len(toWrite)-1]
+		}
+		_, err2 := out.Write(toWrite)
+		mu.Unlock()
+
+		if err != nil {
+			return err
+		}
+		if err2 != nil {
+			return err2
 		}
 	}
 }
